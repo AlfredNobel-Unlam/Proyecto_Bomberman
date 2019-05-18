07ef1fa67e4082a8df487022f9543947e4c11af4
laboratorios <laboratorios@LAB4-21.unlmlab.edu.ar>
2019-05-18 12:44:16 -0300

    bug del class obstaculo eliminado

---
+++ proyecto/src/ObstaculoDestruible.java
@@ -0,0 +1,10 @@
+
+public class ObstaculoDestruible extends ObjetoEnMapa {
+	private boolean destructible = true;
+
+	public ObstaculoDestruible(int posX, int posY) {
+		super(posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ proyecto/src/ObstaculoNoDestruible.java
@@ -0,0 +1,9 @@
+
+public class ObstaculoNoDestruible extends ObjetoEnMapa{
+    private boolean destructible=false;
+	public ObstaculoNoDestruible(int posX, int posY) {
+		super(posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+
+}
